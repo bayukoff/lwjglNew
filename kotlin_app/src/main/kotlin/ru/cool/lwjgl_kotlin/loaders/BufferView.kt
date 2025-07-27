44ef53fc527410081291a4c25283932935a2d030
Coolz <cool56563@yandex.ru>
2025-07-27 11:06:47 +0300

    added model loader, refactored architecture, added skeletal animation support

--- kotlin_app/src/main/kotlin/ru/cool/lwjgl_kotlin/loaders/BufferView.kt
+++
@@ -1,9 +0,0 @@
-package ru.cool.lwjgl_kotlin.loaders
-
-data class BufferView(
-    val buffer: Int,
-    val byteOffset: Int,
-    val byteLength: Int,
-    val byteStride: Int,
-    val target: Int
-)
--- kotlin_app/src/main/kotlin/ru/cool/lwjgl_kotlin/loaders/Model.kt
+++
@@ -1,6 +0,0 @@
-package ru.cool.lwjgl_kotlin.loaders
-
-import ru.cool.lwjgl_kotlin.objects.Mesh
-
-class Model(var meshes: List<Mesh>) {
-}
